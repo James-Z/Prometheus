5b0ea9a03e81819734dffac33d95ffd0b1444727
James-Z <James-Z@users.noreply.github.com>
2017-11-28 16:26:38 +0800

    create class soul, a playerController

---
+++ Source/Prometheus/Player/Soul.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Soul.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROMETHEUS_API ASoul : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
